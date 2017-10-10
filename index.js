75a1e2c4c70d094bc03754cc6989b4b6e8ba5482
Rachel Bernardo <racheljbernardo@gmail.com>
2017-10-10 23:04:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,17 @@ function logShout(string) {
   console.log(string.toUpperCase())
 }
 
-function toWhisper(string) {
+function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
   if (string === lowercase) {
     return("I can\'t hear you!")
+
   } else if (string === uppercase) {
       return("YES INDEED!")
+
     } else if (string === "I love you, Grandma.") {
       return("I love you, too.")
     }
